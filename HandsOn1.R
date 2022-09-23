7c195cc09080bf53755c8e8ec04ffb6142d940ba
Daniel Blázquez <danielblazquez312@gmail.com>
2022-09-23 11:25:23 +0200

    Creamos HandsOn1

---
+++ HandsOn1.R
@@ -0,0 +1,27 @@
+
+# ID SCRIPT ---------------------------------------------------------------
+
+##LENGUAJES DE PROGRAMACIÓN ESTADÍSTICA
+##PROFESOR: CHRISTIAN SUCUZHANAY AREVALO
+##ALUMNO: DANIEL BLÁZQUEZ, EXP 22049209
+##HANDS ON 01
+
+
+# SHORCUTS ----------------------------------------------------------------
+
+##ctrl + l  = clean console
+##ctrl + shift + r = new section
+
+
+# LOADING LIBS ------------------------------------------------------------
+
+install.packages("tidyverse","httr","janitor")
+
+
+
+
+
+
+
+
+
