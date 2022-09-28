645fb8a528fb3f05ae1b01059d5543c99924b501
Daniel Blázquez <danielblazquez312@gmail.com>
2022-09-28 11:38:33 +0200

    segunda clase LPE

--- HandsOn1.R
+++ HandsOn1.R
@@ -13,13 +13,46 @@
 ##ctrl + shift + r = new section
 
 
+# GIT COMMANDS ------------------------------------------------------------
+
+## git init
+##git status
+## git add
+## git commit -m "mensaje"
+## git push -u origin main
+## git pull
+## git branch -myRama
+## git remote add 
+## git merge 
+## git remote add origin (url.git)
+## git clone
+
+
+
+# TERMINAL COMMANDS -------------------------------------------------------
+
+##ls
+##cd
+##pwd
+##mkdir
+##touch
+##nano
+##less
+##cat
+##where/which
+
 # LOADING LIBS ------------------------------------------------------------
 
 install.packages("tidyverse","httr","janitor")
+install.packages("pacman")
+library(tidyverse)
 
 
+# BASIC OPERATORS ---------------------------------------------------------
 
+cristina <- 20
 
+clase_lep <-c('Marta','Emilia','Pablo')
 
 
 
